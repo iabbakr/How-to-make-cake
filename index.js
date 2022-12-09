5f11b7ff4e7fa1c85a15675a0274c1508b2404d1
iabbakr <abubakaribrahim603@gmail.com>
2022-12-09 23:34:25 +0100

    added js and css

---
+++ index.js
@@ -0,0 +1,12 @@
+function setTheme(theme){
+    if (theme === "light"){
+        root.style.setProperty('--bg-color', '#ECF2FF');
+        root.style.setPropert('--text-color', '#2B283A');
+        root.style.setProperty('--title-color', '#4A4E74');
+    }else if (theme === "dark"){
+        root.style.setProperty('--bg-color', '#2B283A');
+        root.style.setProperty('--text-color', 'ECF2FF');
+        root.style.setProperty('--title-color', '#D5D4D8');
+    }
+}
+setTheme("dark");
